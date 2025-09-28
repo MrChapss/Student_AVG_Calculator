3969f05bf19b4bd4a8ee91d4afb38c8536c78d58
Chapss <palmakarsten@gmail.com>
2025-09-29 01:35:24 +0800

    added input

--- SGC.java
+++ SGC.java
@@ -10,6 +10,7 @@ class SGC {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
+        System.out.print("Enter your grade here: ")
         
     }
 }
