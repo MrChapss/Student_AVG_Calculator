7b545cbf4fc32054519dd4aed36482e6581b3939
Chapss <palmakarsten@gmail.com>
2025-09-29 01:31:54 +0800

    added instructions

--- SGC.java
+++ SGC.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+/*
+ * Create a student grade calculator (using loop and arrays)
+ * 1. Bawal AI
+ * 2. Use return statement for returning value
+ * 3. Complete the project with no bugs/error
+ * 
+ */
 class SGC {
     public static void main(String[] args) {
-        //comment muna
-        // 2nd comment
-        // erm
+        Scanner input = new Scanner(System.in);
+        
+        
     }
 }
