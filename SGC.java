1331ba57ac2bbe0c1ff20ff7d93530a90ef08504
Chapss <palmakarsten@gmail.com>
2025-09-30 01:50:14 +0800

    bug at totalAvg, wrong result

--- SGC.java
+++ SGC.java
@@ -4,13 +4,35 @@ import java.util.Scanner;
  * 1. Bawal AI
  * 2. Use return statement for returning value
  * 3. Complete the project with no bugs/error
+ * 4. Additional feature
  * 
  */
 class SGC {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int userHowManySub, userGrades;
+        double totalAvg = 0;
         
-        System.out.print("Enter your grade here: ")
-        int userInput = input.nextInt();
+        // mag add ako ng array na yung size ng array ay naka depende kay user
+        // Asking the user how many he/she have
+        System.out.print("How many subject do you have?: ");
+        userHowManySub = input.nextInt();
+        int[] userHMS = new int [userHowManySub];
+    
+
+        for (int i=0; i < userHMS.length; i++) {
+
+            // User will input their grades in this scanner "userGrades"
+            System.out.print("Input your grades: ");
+            userGrades = input.nextInt();
+
+            //Computation for getting the average based on the given grades
+            totalAvg += userGrades / userHMS.length;
+        }
+        System.out.println("The AVG of your grades is : " + totalAvg);
+        
+        
+    input.close();
+
     }
 }
