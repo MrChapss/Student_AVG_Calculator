5ce4071a56dea823d3548dbc8dd71c232560da24
Chapss <palmakarsten@gmail.com>
2025-09-30 02:40:08 +0800

    fix computation

--- SGC.java
+++ SGC.java
@@ -10,10 +10,11 @@ import java.util.Scanner;
 class SGC {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int userHowManySub, userGrades;
+        int userHowManySub;
         String tryAgain;
-        double totalAvg = 0;
+        double totalAvg = 0, userGrades;
 
+        // adding do while para pwede ulitin yung program unless gusto pa ni user
         do {
 
         // Asking the user how many he/she have
@@ -25,20 +26,22 @@ class SGC {
             for (int i=0; i < userHMS.length; i++) {
 
             // User will input their grades in this scanner "userGrades"
-            System.out.print("Input your grades: ");
-            userGrades = input.nextInt();
+            System.out.printf("Input your grades: ");
+            userGrades = input.nextDouble();
 
             //Computation for getting the average based on the given grades
-            totalAvg += userGrades / userHMS.length;
+            totalAvg += userGrades / (userHMS.length);
             }
+
+            System.out.printf("%nThe AVG of your grades is : " + totalAvg);
             
-            System.out.println("The AVG of your grades is : " + totalAvg);
-            
-            System.out.println("Do you want to try again? (Yes/No): ");
+            System.out.printf("%n%nDo you want to try again? (Yes/No): ");
             tryAgain = input.next();
 
-        } while (tryAgain != "Yes");
-        
+            // add ako ng if incase na wala sa dalawa yung tinype ni user
+            //if 
+
+        } while (tryAgain.equals("No"));
         
         
     input.close();
