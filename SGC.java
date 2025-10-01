df1e8996a872340b231b8f7e56b5bcc7ef4f3d7a
Chapss <palmakarsten@gmail.com>
2025-10-01 14:24:00 +0800

    + retry loop for (Yes/No)

--- SGC.java
+++ SGC.java
@@ -18,7 +18,7 @@ class SGC {
         do {
 
         // Asking the user how many he/she have
-            System.out.print("How many subject do you have?: ");
+            System.out.printf("%nHow many subject do you have?: ");
             userHowManySub = input.nextInt();
             int[] userHMS = new int [userHowManySub];
         
@@ -33,15 +33,15 @@ class SGC {
             totalAvg += userGrades / (userHMS.length);
             }
 
-            System.out.printf("%nThe AVG of your grades is : " + totalAvg);
+            System.out.printf("%nThe AVG of your grade/s is : " + totalAvg);
             
             System.out.printf("%n%nDo you want to try again? (Yes/No): ");
             tryAgain = input.next();
 
             // add ako ng if incase na wala sa dalawa yung tinype ni user
-            if (!tryAgain.equals("Yes") && !tryAgain.equals("No")) {
-                System.out.printf("%n%nINVALID CHOICE!");
-                break;
+            while (!tryAgain.equals("Yes") && !tryAgain.equals("No")) {
+                System.out.printf("Do you want to try again? (Yes/No): ");
+                tryAgain = input.next();
             } 
 
         } while (!tryAgain.equals("No"));
@@ -51,3 +51,9 @@ class SGC {
 
     }
 }
+// TO-DO
+// 1. EDIT THE "ADD DO WHILE" COMMENT INTO ADDED TRY AGAIN OPTION
+// 2. MAG LOOP YUNG IF KAHIT INVALID NILAGAY NI USER
+// 3. CLEAN THE CODE
+// 4. MAKE IT INTO RETURN (I THINK WAG MUNA)
+// 5. POLISH THE CODE
