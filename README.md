4a45cd9cbd220431a45be918d0c08052a2e7632e
Chapss <palmakarsten@gmail.com>
2025-10-08 23:43:20 +0800

    Rules and Description

---
+++ README.md
@@ -0,0 +1,5 @@
+Create a student average grade calculator
+1. No use of AI
+2. Using loops and array
+3. Using KISS or DRY method in coding it
+4. Complete the project with bugs/error
--- SGC.java
+++ SGC.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
-/*
- * Create a student grade calculator (using loop and arrays)
- * 1. Bawal AI
- * 2. Use return statement for returning value
- * 3. Complete the project with no bugs/error
- * 4. Additional feature
- * 
- */
 class SGC {
     public static void main(String[] args) {
+        // initialize the variables
         Scanner input = new Scanner(System.in);
         int userHowManySub;
         String tryAgain;
         double totalAvg = 0, userGrades;
 
-        // adding do while para pwede ulitin yung program unless gusto pa ni user
+        // do while to repeat the program if the user input "Yes"
         do {
 
         // Asking the user how many he/she have
@@ -22,39 +15,36 @@ class SGC {
             userHowManySub = input.nextInt();
             int[] userHMS = new int [userHowManySub];
         
-
+            // container of the user grades equivalent on how many subject did user put
             for (int i=0; i < userHMS.length; i++) {
 
             // User will input their grades in this scanner "userGrades"
-            System.out.printf("Input your grades: ");
+            System.out.print("Input your grades: ");
             userGrades = input.nextDouble();
 
-            // merong BUG dto kasi kapag umuulit nag increase din yung value sa total
             //Computation for getting the average based on the given grades
-            totalAvg += userGrades / (userHMS.length);
+            totalAvg += userGrades / userHMS.length;
             }
-
+            // output
             System.out.printf("%nThe AVG of your grade/s is : " + totalAvg);
-            
-            System.out.printf("%n%nDo you want to try again? (Yes/No): ");
-            tryAgain = input.next();
 
-            // add ako ng if incase na wala sa dalawa yung tinype ni user
-            while (!tryAgain.equals("Yes") && !tryAgain.equals("No")) {
-                System.out.printf("Do you want to try again? (Yes/No): ");
+            //reset the value of totalAvg to 0
+            totalAvg = 0;
+
+            //asking the user if they want to re-try the program
+            while(true){
+                System.out.printf("%nDo you want to try again? (Yes/No): ");
                 tryAgain = input.next();
-            } 
 
-        } while (!tryAgain.equals("No"));
-        
-        
-    input.close();
-
+                if (!tryAgain.equalsIgnoreCase("Yes") && !tryAgain.equalsIgnoreCase("No")) {
+                    System.out.println("Please choose from (Yes/No) only!");
+                }
+                if (tryAgain.equalsIgnoreCase("No")) {
+                    break;
+                }
+            }
+            // if no, it will end the program (since condition will be false == stop)
+        } while (!tryAgain.equalsIgnoreCase("No"));
+        input.close();
     }
 }
-// TO-DO
-// 1. EDIT THE "ADD DO WHILE" COMMENT INTO ADDED TRY AGAIN OPTION
-// 2. MAG LOOP YUNG IF KAHIT INVALID NILAGAY NI USER
-// 3. CLEAN THE CODE
-// 4. MAKE IT INTO RETURN (I THINK WAG MUNA)
-// 5. POLISH THE CODE
